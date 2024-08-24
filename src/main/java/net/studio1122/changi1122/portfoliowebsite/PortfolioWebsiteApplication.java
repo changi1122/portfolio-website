f5197c14a95244d8fe9f12119dc66cab91412a53
Woochang Lee <changi112242@gmail.com>
2024-08-25 01:34:46 +0900

    init: Spring boot 프로젝트 생성

---
+++ src/main/java/net/studio1122/changi1122/portfoliowebsite/PortfolioWebsiteApplication.java
@@ -0,0 +1,13 @@
+package net.studio1122.changi1122.portfoliowebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/studio1122/changi1122/portfoliowebsite/PortfolioWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package net.studio1122.changi1122.portfoliowebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
