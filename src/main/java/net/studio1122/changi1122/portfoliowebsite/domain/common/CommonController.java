14d03cd80e5a5f5746ea1631fa07d3d94cf0b86c
Woochang Lee <changi112242@gmail.com>
2024-08-27 18:13:50 +0900

    ui: 블로그 페이지 퍼블리싱

--- src/main/java/net/studio1122/changi1122/portfoliowebsite/domain/common/CommonController.java
+++ src/main/java/net/studio1122/changi1122/portfoliowebsite/domain/common/CommonController.java
@@ -2,6 +2,7 @@ package net.studio1122.changi1122.portfoliowebsite.domain.common;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class CommonController {
@@ -9,5 +10,11 @@ public class CommonController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    /* For test */
+    @GetMapping("/blog/{page}")
+    public String blog(@PathVariable Integer page) {
+        return "blog";
     }
 }
