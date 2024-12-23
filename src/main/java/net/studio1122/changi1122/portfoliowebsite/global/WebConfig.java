9a9f5ee28b97b51a29cb376abb7daf5fc025e41e
Woochang Lee <changi112242@gmail.com>
2024-12-23 20:05:03 +0900

    feat: Error Page 추가

--- src/main/java/net/studio1122/changi1122/portfoliowebsite/global/WebConfig.java
+++ src/main/java/net/studio1122/changi1122/portfoliowebsite/global/WebConfig.java
@@ -21,6 +21,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new GetOnlyInterceptor())
                 .order(2)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/login", "/logout", "/signup");
+                .excludePathPatterns("/error", "/login", "/logout", "/signup");
     }
 }
--- src/main/java/net/studio1122/changi1122/portfoliowebsite/web/home/HomeController.java
+++ src/main/java/net/studio1122/changi1122/portfoliowebsite/web/home/HomeController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
